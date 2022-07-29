2636b3f98e6e7bbc32d766237855314abeb13a82
NewYorkKim <newyorkkim1030@gmail.com>
2022-07-29 17:05:13 +0900

    TIL: 2022-07-29

--- Application/NomadCoders_ReactJS/react-for-beginners/src/routes/Detail.js
+++ Application/NomadCoders_ReactJS/react-for-beginners/src/routes/Detail.js
@@ -1,20 +1,39 @@
-import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
     const { id } = useParams()
+    const [loading, setLoading] = useState(true);
+    const [detail, setDetail] = useState([]);
     const getMovie = async () => {
         const json = await (
-            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+            await fetch(
+                `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+                )
         ).json();
-        console.log(json);
+        setDetail(json.data.movie);
+        setLoading(false);
     }
     useEffect(() => {
         getMovie();
     }, []);
     return (
-        <h1>Detail</h1>
+        <div>
+            {loading ? (
+                <h1>Laoding...</h1>
+                ) : (
+                    <div>
+                        <img src={detail.medium_cover_image} alt={detail.title} />
+                        <h1>{detail.title}</h1>
+                        <ul>
+                            <li>year: {detail.year}</li>
+                            <li>rating: {detail.rating}</li>
+                            <li>like count: {detail.like_count}</li>
+                        </ul>
+                        <p>{detail.description_full}</p>
+                    </div>
+            )}
+        </div>
     );
 }
 
