a922fe1570065960b3c29c97fc50b46e893472aa
NewYorkKim <newyorkkim1030@gmail.com>
2022-07-31 23:42:51 +0900

    TIL: 2022/07/31

--- Application/NomadCoders_ReactNative/NomadWeather/App.js
+++ Application/NomadCoders_ReactNative/NomadWeather/App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, ScrollView, Dimensions, ActivityIndicator } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "{API_KEY}";
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunerstorm: "lightning"
+};
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async() => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+    const {coords:{ latitude, longitude }} = await Location.getCurrentPositionAsync({accuracy:5});
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude }, 
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`)
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect (() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Hello! I made a RN App!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{ city }</Text>
+      </View>
+      <ScrollView 
+        pagingEnabled
+        horizontal
+        // indicatorStyle="white"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" style={{marginTop: 10}} size="large" />
+          </View>
+          ) : ( 
+            days.map((day, index) => 
+              <View key={index} style={styles.day}>
+                <View style={{ 
+                    flexDirection: "row", 
+                    alignItems: "flex-end",
+                    width: "100%",
+                    justifyContent: "space-between"
+                  }}>
+                  <Text style={styles.temp}>
+                    {parseFloat(day.temp.day).toFixed(1)}
+                  </Text>
+                  <Fontisto name={icons[day.weather[0].main]} size={68} color="white" />
+                </View>
+                <Text style={styles.desc}>{day.weather[0].main}</Text>
+                <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+              </View>
+            )
+        )}
+      </ScrollView>
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    backgroundColor: "blue"
   },
-});
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    color: "white",
+    fontSize: 68,
+    fontWeight: "500"
+  },
+  weather : {
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "flex-start",
+    paddingHorizontal: 20
+  },
+  temp: {
+    marginTop: 50,
+    fontWeight: "600",
+    color: "white",
+    fontSize: 100
+  },
+  desc: {
+    marginTop: -10,
+    color: "white",
+    fontSize: 30,
+    fontWeight: "500"
+  },
+  tinyText: {
+    marginTop: -5,
+    fontSize: 25,
+    color: "white",
+    fontWeight: "500"
+  }
+})
