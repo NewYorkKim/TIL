508c8de98bf97f0d10261fcaec3b55184546bae6
NewYorkKim <newyorkkim1030@gmail.com>
2022-08-23 13:40:00 +0900

    TIL: 2022/08/23

--- Application/yeosul_firebase/App.js
+++ Application/yeosul_firebase/App.js
@@ -1,9 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import firebase from "firebase";
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 export default function App() {
-  const db = firebase.firestore();
+  const firebaseConfig = {
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  
+  async function getSulData(db) {
+    const sulCol = collection(db, 'sool');
+    const sulSnapShot = await getDocs(sulCol);
+    const sulList = sulSnapShot.docs.map(doc => doc.data());
+    console.log(sulList);
+  }
+
+  useEffect (() => {
+    getSulData(db);
+  }, []);
 
   return (
     <View style={styles.container}>
