dbbfd668a00a31f7c18e4a26174075f5af821a95
NewYorkKim <newyorkkim1030@gmail.com>
2023-06-24 16:35:06 +0900

    TIL: 2023/06/24

--- Algorithm/Solve/BOJ_1010.py
+++ Algorithm/Solve/BOJ_1010.py
@@ -1,15 +1,8 @@
-import sys
+import sys, math
 input = sys.stdin.readline
 
 t = int(input())
 
 for _ in range(t):
     n, m = map(int, input().split())
-    cases = []
-    
-    for i in range(n):
-        for j in range(i, m):
-            cases.append((i+1, j+1))
-    
-    print(len(cases))
-            
+    print(math.comb(m, n))
