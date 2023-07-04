40e5d3ceac9726120ac041698b1aa82c95182a84
NewYorkKim <newyorkkim1030@gmail.com>
2023-07-04 15:40:08 +0900

    TIL: 2023/07/04

---
+++ Algorithm/Solve/PRO_181840.py
@@ -0,0 +1,4 @@
+def solution(num_list, n):
+    answer = n in num_list
+    
+    return +answer
---
+++ Algorithm/Solve/PRO_181843.py
@@ -0,0 +1,4 @@
+def solution(my_string, target):
+    answer = target in my_string
+    
+    return +answer
