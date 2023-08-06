fbbb21916a8469ed58460215dda1363c1a5f2fb9
NewYorkKim <newyorkkim1030@gmail.com>
2023-08-06 17:46:34 +0900

    TIL: 2023/08/06

---
+++ Application/iOS_Tutorial/FlipTutorial/FlipTutorial/FlipTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipTutorialApp.swift
+//  FlipTutorial
+//
+//  Created by Nayeon Kim on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct FlipTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
