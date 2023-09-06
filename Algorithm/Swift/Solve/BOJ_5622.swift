95abec626533c924d6dd594352bd86720fa2b850
NewYorkKim <newyorkkim1030@gmail.com>
2023-09-06 11:50:37 +0900

    TIL: 2023/09/06

---
+++ Algorithm/Swift/Solve/BOJ_5622.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Solve
+//
+//  Created by Nayeon Kim on 2023/09/06.
+//
+
+import Foundation
+
+//let dial = [3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10]
+//
+//let chs = readLine()!.map { dial[Int($0.asciiValue!) - 65] }.reduce(0, +)
+//print(chs)
