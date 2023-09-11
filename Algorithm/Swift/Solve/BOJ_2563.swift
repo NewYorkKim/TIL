3b0cbb7bb2ca9a62b2a356f58388a88553fc8a23
NewYorkKim <newyorkkim1030@gmail.com>
2023-09-11 16:08:08 +0900

    [TIL] 2023/09/11

---
+++ Algorithm/Swift/Solve/BOJ_2563.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Solve
+//
+//  Created by Nayeon Kim on 2023/09/11.
+//
+
+import Foundation
+
+//var area = Array(repeating: (Array(repeating: 0, count: 100)), count: 100)
+//let n = Int(readLine()!)!
+//
+//for _ in 0..<n {
+//    let nums = readLine()!.split { $0 == " " }.map { Int($0)! }
+//    let x = nums[0] - 1, y = nums[1] - 1
+//
+//    for i in 0..<10 {
+//        for j in 0..<10 {
+//            area[x + i][y + j] = 1
+//        }
+//    }
+//}
+//
+//let result = area.flatMap { $0 }.reduce(0, +)
+//print(result)
---
+++ Algorithm/Swift/Solve/BOJ_2745.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Solve
+//
+//  Created by Nayeon Kim on 2023/09/11.
+//
+
+import Foundation
+
+//let nums = readLine()!.split { $0 == " " }
+//let n = nums[0], b = Int(nums[1])!
+//
+//print(Int(n, radix: b)!)
