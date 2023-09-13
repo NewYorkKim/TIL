075456bc9549bd4e3da0dc3e920eb0cb38f3a421
NewYorkKim <newyorkkim1030@gmail.com>
2023-09-13 19:57:09 +0900

    [TIL] 2023/09/13

---
+++ Algorithm/Swift/Solve/BOJ_1193.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Solve
+//
+//  Created by Nayeon Kim on 2023/09/13.
+//
+
+import Foundation
+
+//let x = Int(readLine()!)!
+//var line = 0, end = 0
+//
+//while x > end {
+//    line += 1
+//    end += line
+//}
+//
+//let diff = end - x
+//
+//if line % 2 == 0 {
+//    let i = line - diff, j = diff + 1
+//    print("\(i)/\(j)")
+//} else {
+//    let i = diff + 1, j = line - diff
+//    print("\(i)/\(j)")
+//}
---
+++ Algorithm/Swift/Solve/BOJ_2869.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Solve
+//
+//  Created by Nayeon Kim on 2023/09/13.
+//
+
+import Foundation
+
+//let nums = readLine()!.split { $0 == " " }.map { Int($0)! }
+//let a = nums[0], b = nums[1], v = nums[2]
+//
+//let day = Int(ceil(1 + Double(v - a) / Double(a - b)))
+//
+//print(day)
