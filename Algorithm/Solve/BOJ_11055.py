8b32e8f0cef15f737a85abfe98edaf2d55fd334a
NewYorkKim <newyorkkim1030@gmail.com>
2023-09-21 17:58:33 +0900

    TIL: 2023/09/21

--- Algorithm/Solve/BOJ_11055.py
+++ Algorithm/Solve/BOJ_11055.py
@@ -5,8 +5,11 @@ dp = [1] * a
 ans = [seq[0]]
 
 for i in range(1, a):
+    tmp = 0
     for j in range(i+1):
         if seq[i] > seq[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+            tmp = max(tmp, max(seq[i-1] + seq[i], seq[i-1] + seq[j]))
+    dp[i] = tmp
  
 print(ans)
+print(dp)
